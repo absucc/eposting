7b103ea084d4e886bbb89dadabd070e66ac9e19f
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-02 16:42:51 -0300

    Update images.php

--- config/images.php
+++ config/images.php
@@ -1,19 +1,21 @@
-<?php
-/* First URL of the type of media (image/video), then the title/text of the content, later an image, and, if the content is a video, the video file
-The first thing in the variable is the oldest publication
-Please use correctly the ","
-Use the following example to make your own images.php
-Good luck! */
-$data = [
+<?php $data = [
   [
-    "image",
+    "image", // type 
     'My face when<br>My face when the girl who proposed to me 5 minutes ago publishes "Challenge completed"<br>#espanol #xdnt',
-    "https://cdn.discordapp.com/attachments/819742251453775912/827300161482260510/unknown.png"
+    "https://cdn.discordapp.com/attachments/819742251453775912/827300161482260510/unknown.png",
+    "absucc"
   ],
   [
     "video",
     "by Percha TV#[CENSURED] (Discord)",
     "https://raw.githubusercontent.com/absucc/ecdn/main/2.PNG",
-    "https://shiba.banana.dog/media_attachments/files/010/750/822/original/1a242bf299edec45.mp4"
+    "https://shiba.banana.dog/media_attachments/files/010/750/822/original/1a242bf299edec45.mp4",
+    "absucc"
+  ],
+  [
+    "image",
+    "She's right",
+    "https://shiba.banana.dog/media_attachments/files/010/689/087/original/ad9e7704aa85352b.png",
+    "absucc"
   ]
 ]; ?>
