8c974fdde697cf29a2160cdfe0b8a6392a0d61ad
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-02 16:03:43 -0300

    Update footer.php

--- core/includes/footer.php
+++ core/includes/footer.php
@@ -1,6 +1,6 @@
 </main>
 <br><hr>
-<footer>Based on <a href="https://absucc.github.io/nanoiweb/">Nanoiweb</a> v1.0.2 | <a rel="me" href="https://banana.dog/@e"><i class="fa fa-mastodon"></i></a></footer>
+<footer>Based on <a href="https://absucc.github.io/nanoiweb/">Nanoiweb</a> v1.0.2></footer>
 </div>
 </body>
 </html>
