a0a153a455b4d8ada0fa2bd55c9aeb96aba57238
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-02 17:06:09 -0300

    Update images.php

--- config/images.php
+++ config/images.php
@@ -1,21 +1,21 @@
 <?php $data = [
   [
-    "image", // type 
+    "image",
     'My face when<br>My face when the girl who proposed to me 5 minutes ago publishes "Challenge completed"<br>#espanol #xdnt',
-    "https://cdn.discordapp.com/attachments/819742251453775912/827300161482260510/unknown.png",
-    "absucc"
+    "absucc",
+    "https://cdn.discordapp.com/attachments/819742251453775912/827300161482260510/unknown.png"
   ],
   [
     "video",
     "by Percha TV#[CENSURED] (Discord)",
+    "absucc",
     "https://raw.githubusercontent.com/absucc/ecdn/main/2.PNG",
-    "https://shiba.banana.dog/media_attachments/files/010/750/822/original/1a242bf299edec45.mp4",
-    "absucc"
+    "https://shiba.banana.dog/media_attachments/files/010/750/822/original/1a242bf299edec45.mp4"
   ],
   [
     "image",
     "She's right",
-    "https://shiba.banana.dog/media_attachments/files/010/689/087/original/ad9e7704aa85352b.png",
-    "absucc"
+    "absucc",
+    "https://shiba.banana.dog/media_attachments/files/010/689/087/original/ad9e7704aa85352b.png"
   ]
 ]; ?>
