6ae16d13f72fe711e9e39514ad0eba6f8d6a5fc2
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-02 16:31:03 -0300

    Update images.php

--- config/images.php
+++ config/images.php
@@ -9,6 +9,11 @@ $data = [
     "image",
     'My face when<br>My face when the girl who proposed to me 5 minutes ago publishes "Challenge completed"<br>#espanol #xdnt',
     "https://cdn.discordapp.com/attachments/819742251453775912/827300161482260510/unknown.png"
+  ],
+  [
+    "video",
+    "https://raw.githubusercontent.com/absucc/ecdn/main/2.PNG",
+    "https://shiba.banana.dog/media_attachments/files/010/750/822/original/1a242bf299edec45.mp4"
   ]
 ];
 ?>
