d6e7b86f44f5ff6624de803b81bc2d787055f28d
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-04 14:17:10 -0300

    Update json_api.php

--- core/json_api.php
+++ core/json_api.php
@@ -2,5 +2,5 @@
 if(isset($_GET["random"])){
   $r = rand(count($data));
   if($data[$r][0]=="video"){$video=",'video':'".$data[$r][4]."'";}else{$video="";}
-  echo "{'type':'video','image':'".$data[$r][3]."'$video}";
+  echo "{'type':'video','uploader':'".$data[$r][2]."','image':'".$data[$r][3]."'$video}";
 } ?>
