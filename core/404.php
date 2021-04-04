8de0edacc01e7beb687ba0976a14083d4ee8ebdd
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-04 13:55:52 -0300

    Update 404.php

--- core/404.php
+++ core/404.php
@@ -1,6 +1,6 @@
 <?php include "config/general.php";
 $pub=True;
-$webtitle="404";
+$webtitle="404 | ".$WEBSITE_NAME;
 include "core/includes/header.php";
 echo "<h1>Page not found</h1>";
 include "core/includes/footer.php";
