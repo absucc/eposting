ae2b16feb0b38fc905ab3c79112c9d481ee1bda6
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-03 22:43:40 -0300

    Update general.php

--- config/general.php
+++ config/general.php
@@ -2,7 +2,13 @@
 $WEBSITE_NAME = "eposting";
 $WEBSITE_PROFILE_URL = "https://shiba.banana.dog/accounts/avatars/000/177/070/original/4129222e7ab86b6a.png";
 $WEBSITE_WEBSITE = "";
-$WEBSITE_DESCRIPTION = "";
+$wdrand = 1;
+if($wdrand==1){
+  $viprand = rand(1,9);
+  if($viprand==1){$achad="Dee";}elseif($viprand==2){$achad="Gargron";}elseif($viprand==3){$achad="Stux";}elseif($viprand==4){$achad="Mojang Studios";}elseif($viprand==5){$achad="moot";}elseif($viprand==6){$achad="Zeta";}elseif($viprand==7){$achad="H";}elseif($viprand==8){$achad="G";}elseif($viprand==9){$achad="Ben Awad";}elseif($viprand==10){$achad="Tildeverse";}elseif($viprand==11){$achad="Absucc";}
+  $WEBSITE_DESCRIPTION="$achad is chad";
+}
+//$WEBSITE_DESCRIPTION = "";
 $WEBSITE_EMAIL = "";
 $WEBSITE_HTACCESS = False;
 
