5b3b4add5bc8ad393843b4711f421b2ee290d754
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-04 10:32:55 -0300

    Update general.php

--- config/general.php
+++ config/general.php
@@ -2,12 +2,13 @@
 $WEBSITE_NAME = "eposting";
 $WEBSITE_PROFILE_URL = "https://shiba.banana.dog/accounts/avatars/000/177/070/original/4129222e7ab86b6a.png";
 $WEBSITE_WEBSITE = "";
-$wdrand = rand(1,2);
+$wdrand = rand(1,3);
 if($wdrand==1){
   $viprand = rand(1,10);
   if($viprand==1){$achad="Dee";}elseif($viprand==2){$achad="Gargron";}elseif($viprand==3){$achad="Stux";}elseif($viprand==4){$achad="Mojang Studios";}elseif($viprand==5){$achad="moot";}elseif($viprand==6){$achad="Zeta";}elseif($viprand==7){$achad="H";}elseif($viprand==8){$achad="G";}elseif($viprand==9){$achad="Ben Awad";}elseif($viprand==10){$achad="Tildeverse";}elseif($viprand==11){$achad="Absucc";}
   $WEBSITE_DESCRIPTION="$achad is chad";
-}elseif($wdrand==2){$WEBSITE_DESCRIPTION="Aaaaaaaaaaaaaaaaa";}
+}elseif($wdrand==2){$WEBSITE_DESCRIPTION="Aaaaaaaaaaaaaaaaa";
+}elseif($wdrand==3){$WEBSITE_DESCRIPTION="Lain <span style='text-decoration:line-through'>chad</span> chan(.org)";}
 //$WEBSITE_DESCRIPTION = "";
 $WEBSITE_EMAIL = "";
 $WEBSITE_HTACCESS = False;
