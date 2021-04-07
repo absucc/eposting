e829251b4f9f9b8a2e74c651978a95ccc53b72f3
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-07 20:52:37 -0300

    Delete json_api.php

--- core/json_api.php
+++
@@ -1,6 +0,0 @@
-<?php include "config/images.php"; header("Content-type: application/json");
-if(isset($_GET["random"])){
-  $r = rand(count($data));
-  if($data[$r][0]=="video"){$video=",'video':'".$data[$r][4]."'";}else{$video="";}
-  echo "{'type':'video','uploader':'".$data[$r][2]."','image':'".$data[$r][3]."'$video}";
-} ?>
