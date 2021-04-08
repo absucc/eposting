846e5f2ee0b0376064763e11bff8b1ae650a2ec1
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-08 16:55:14 -0300

    Create about.php

---
+++ core/about.php
@@ -0,0 +1,13 @@
+<?php include "config/general.php"; include "config/images.php"; include "includes/header.php"; $pub=False; ?>
+<div>
+<h1>Eposting</h1>
+  <h2>Creator</h2>
+  <p>Lucas "<a href="https://l64.repl.co">Absucc</a>" ???</p>
+  <h2>Staff</h2>
+  <p>Lucas "<a href="https://l64.repl.co">Absucc</a>" ???</p>
+  <p>andil19</p>
+<p>
+  Software based on <a href="https://absucc.github.io/nanoiweb/">Nanoiweb</a> v1.0.3 (by <a href="https://l64.repl.co">Absucc</a>)<br>&copy;2021 Eposting<br>Under the <a href="https://choosealicense.com/licenses/mit/">MIT License</a>
+</p>
+</div>
+<?php include "includes/footer.php" ?>
