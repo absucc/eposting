9a920b2e87b10ce2d624cec766c7fcb626c737fb
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-08 17:02:11 -0300

    Update about.php

--- core/about.php
+++ core/about.php
@@ -1,11 +1,11 @@
-<?php include "config/general.php"; include "config/images.php"; include "includes/header.php"; $pub=False; ?>
+<?php include "config/general.php"; include "config/images.php"; include "includes/header.php"; $pub=True; ?>
 <div>
 <h1>Eposting</h1>
-  <h2>Creator</h2>
-  <p>Lucas "<a href="https://l64.repl.co">Absucc</a>" ???</p>
-  <h2>Staff</h2>
-  <p>Lucas "<a href="https://l64.repl.co">Absucc</a>" ???</p>
-  <p>andil19</p>
-<p>Software based on <a href="https://absucc.github.io/nanoiweb/">Nanoiweb</a> v1.0.3 (by <a href="https://l64.repl.co">Absucc</a>)<br>&copy;2021 Eposting<br>Under the <a href="https://choosealicense.com/licenses/mit/">MIT License</a><br><a href="https://github.com/eposting/eposting">Source code</a></p>
+<h2>Creator</h2>
+<p>Lucas "Absucc" ???</p>
+<h2>Staffs</h2>
+<p>Lucas "Absucc" ???</p>
+<p>andil19</p>
+<p>Software based on <a href="https://absucc.github.io/nanoiweb/">Nanoiweb</a> v1.0.2 (by <a href="https://l64.repl.co">Absucc</a>)<br>&copy;2021 Eposting<br>Under the <a href="https://choosealicense.com/licenses/mit/">MIT License</a><br><a href="https://github.com/eposting/eposting">Source code</a></p>
 </div>
 <?php include "includes/footer.php" ?>
