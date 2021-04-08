b70abe15cdac4e5dc1883473c7e8fc17a7c2a41f
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-08 17:03:59 -0300

    Update footer.php

--- core/includes/footer.php
+++ core/includes/footer.php
@@ -1,6 +1,6 @@
 </main>
 <br><hr>
-<footer>&copy;2021 Eposting - <a href="?about">About</a></footer>
+<footer><a href="https://git.io/JYAyC">Source code</a> - <a href="?about">About</a></footer>
 </div>
 </body>
 </html>
