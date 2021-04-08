90e9c9161d839af031f86d740d30bff461f65765
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-08 17:15:25 -0300

    Update about.php

--- core/about.php
+++ core/about.php
@@ -2,10 +2,13 @@
 <div>
 <h1>Eposting</h1>
 <h2>Creator</h2>
-<p>Lucas "Absucc" ???</p>
+<p>Lucas "Absucc" ???</p><br>
 <h2>Staffs</h2>
 <p>Lucas "Absucc" ???</p>
-<p>andil19</p>
-<p>Software based on <a href="https://absucc.github.io/nanoiweb/">Nanoiweb</a> v1.0.2 (by <a href="https://l64.repl.co">Absucc</a>)<br>&copy;2021 Eposting<br>Under the <a href="https://choosealicense.com/licenses/mit/">MIT License</a><br><a href="https://github.com/eposting/eposting">Source code</a></p>
+<p>andil19</p><br>
+<p>Software based on <a href="https://absucc.github.io/nanoiweb/">Nanoiweb</a> v1.0.3 (by <a href="https://l64.repl.co">Absucc</a>)</p>
+<p>&copy;2021 Eposting</p>
+<p>Under the <a href="https://choosealicense.com/licenses/mit/">MIT License</a></p>
+<p><a href="https://github.com/eposting/eposting">Source code</a></p>
 </div>
 <?php include "includes/footer.php" ?>
