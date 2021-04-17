115520f84488bd6ed870bc669c5663634488f334
Absucc <57920717+absucc@users.noreply.github.com>
2021-04-17 17:26:18 -0300

    Update images.php

--- config/images.php
+++ config/images.php
@@ -5,10 +5,6 @@
   ["image","Youtuber comments in the video of someone<br>Their followers: What are you doin' here?!!","absucc","https://shiba.banana.dog/media_attachments/files/010/650/217/original/710a35c3afbc1ddd.png"],
   ["image","Disturbing","absucc","https://shiba.banana.dog/media_attachments/files/010/682/658/original/a135deef7a8890aa.jpg"],
   ["image","Me: *fails to press the (X) button for 1 millimeter*<br>My phone: We're goin' to the Play Store, dude","absucc","https://shiba.banana.dog/media_attachments/files/010/650/201/original/d91ac357daeb0037.png"],
-  [
-    "image",
-    "FreeFire was created by the goverment to see how much poor people are in the world<br>Credits: u/Broken_Micy (Reddit)",
-    "absucc",
-    "https://github.com/eposting/ecdn/blob/main/%F0%9F%A4%A0?raw=true"
-  ]
+  ["image","FreeFire was created by the goverment to see how much poor people are in the world<br>Credits: u/Broken_Micy (Reddit)","absucc","https://github.com/eposting/ecdn/blob/main/%F0%9F%A4%A0?raw=true"],
+  ["image","F*ck\nby u/fffffffffeverdream (Reddit)","absucc","https://i.redd.it/n2k4yiycjot61.jpg"]
 ]; ?>
