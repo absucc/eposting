615fbcbef0fc7acf454b4d1ecac55c9a9fea0e8e
Absucc <57920717+absucc@users.noreply.github.com>
2021-05-09 19:16:59 -0300

    Update images.php

--- config/images.php
+++ config/images.php
@@ -1,7 +1,7 @@
 <?php $data = [
   ["image",'My face when<br>My face when the girl who proposed to me 5 minutes ago publishes "Challenge completed"<br>#espanol #xdnt',"absucc","https://cdn.discordapp.com/attachments/819742251453775912/827300161482260510/unknown.png"],
   ["video","by Percha TV#[CENSURED] (Discord)","absucc","https://raw.githubusercontent.com/absucc/ecdn/main/2.PNG","https://shiba.banana.dog/media_attachments/files/010/750/822/original/1a242bf299edec45.mp4"],
-  ["image","","",""],
+  ["deleted","","",""],
   ["image","Youtuber comments in the video of someone<br>Their followers: What are you doin' here?!!","absucc","https://shiba.banana.dog/media_attachments/files/010/650/217/original/710a35c3afbc1ddd.png"],
   ["image","Disturbing","absucc","https://shiba.banana.dog/media_attachments/files/010/682/658/original/a135deef7a8890aa.jpg"],
   ["image","Me: *fails to press the (X) button for 1 millimeter*<br>My phone: We're goin' to the Play Store, dude","absucc","https://shiba.banana.dog/media_attachments/files/010/650/201/original/d91ac357daeb0037.png"],
